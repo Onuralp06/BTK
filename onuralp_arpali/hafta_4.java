37fe4a54eb223195323087b430631d82dd6f9417
Onuralp APRALI <100523614+Onuralp06@users.noreply.github.com>
2022-03-26 11:48:53 +0300

    Add files via upload

---
+++ onuralp_arpali/hafta_4.java
@@ -0,0 +1,97 @@
+package onuralp_arpali;
+
+import java.util.Scanner;
+
+public class hafta_4 {
+
+	public static void main(String[] args) {
+		/*int a = 0;
+		while (a <5 ) {
+			System.out.println("A'nýn deðeri: "+a+" *");
+			a++;
+			if (a==3)
+				break;
+			else
+			continue
+		}*/
+		// Kullanýcý ismini giriyor döngüler ile alt alta yazýn
+		/*Scanner onur = new Scanner(System.in);
+		String isim = onur.next();
+		int sayac = 0;
+		while (sayac < isim.length()) {
+			System.out.println(isim.charAt(sayac));
+				sayac++;*/
+		
+		//Kullanýcýnýn isimin tersten yazdýrmak
+		/*Scanner onur = new Scanner(System.in);
+		String isim = onur.next();
+		int sayac = isim.length()-1;
+			while(sayac >= 0 ) {
+				System.out.println(isim.charAt(sayac));
+				sayac--;		
+			}*/
+		//Kullanýcý bir sayý girecek 0'dan 10'a kadar toplamasýný yapmasý gerekmektedir. sonra faktoriyelini yapacaðýz
+		/*Scanner onur = new Scanner(System.in);
+		int sayi = onur.nextInt();
+		int sayac = 0;
+		int toplam = 0;
+		int carpim = 1;
+		
+		while(sayac <= sayi) {
+			toplam += sayac;
+			if (sayac != 0)
+				carpim = sayac;
+			sayac++;
+		}
+		System.out.println("Toplamlarý: "+toplam);
+		System.out.println("Çarpýmlarý:" +carpim);*/
+		
+		//Kullanýcýnýn girdiði sayýyý aritmatik ortalamasýný bulunuz.
+		/*Scanner onur = new Scanner(System.in);
+		int sayi = onur.nextInt();
+		int sayac = 0;
+		int toplam = 0;
+		
+		while(sayac <= sayi) {
+			toplam += sayac;
+			sayac++;
+		}
+		System.out.println("Toplamlarý: "+toplam);
+		double sonuc = (double)toplam/sayi;
+		System.out.println("Aritmatik Ortalama:" +sonuc);*/
+		
+		/*Scanner onur = new Scanner(System.in);
+		int sayac = 0;
+		int not = 0;
+		int toplam = 0;
+		while(true) {
+		System.out.print("Lütfen "+(sayac+1)+" Öðrencinizin notunu giriniz!");
+		System.out.print(" Çýkmak için -1 giriniz !");
+		not = onur.nextInt();
+		if(not == -1)
+			break;
+		toplam += not ;	
+		sayac++;
+		}
+		double ortalama = toplam/(double)sayac;	
+		System.out.println(sayac+"kadar öðrencinin ortalamasý: "+ortalama);*/
+		
+		//For döngüsü
+		/*for (int i=0; i<10;i++) {
+			System.out.println(i);*/
+		//Ýç Ýçe Döngüler
+		
+		/*Scanner onur = new Scanner(System.in);
+		int sayi = onur.nextInt();
+		for (int i=0;i<sayi;i++) {
+			for(int j=0;j<sayi;j++) {
+				System.out.print("*");
+			}
+			System.out.println();*/
+		//bir string alýp palidrom
+		Scanner onur = new Scanner(System.in);
+
+	        }
+	    }
+
+
